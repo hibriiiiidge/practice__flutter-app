9d520e7ac2d1fb8b617d3fd5ec40b880fc594af9
Yasunori Takahashi <hibriiiiidge@gmail.com>
2023-08-08 23:24:35 +0900

    Change body to ListView

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo2',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -42,21 +42,15 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Flutter2'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: const [
+          Text('Flutter2'),
+          Text('Flutter3'),
+          Text('Flutter4'),
+          Text('Flutter5'),
+        ]
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
